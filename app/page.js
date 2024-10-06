c6c0a9d647312174b20df008da02817764fb41a9
Gaurav-2707 <gauravsrivastava2707@gmail.com>
2024-10-06 13:51:03 +0530

    commit

--- app/page.js
+++ app/page.js
@@ -4,30 +4,41 @@ export default function Home() {
   return (
     <>
       <div className="bg-[#e3e3e3] h-20">
-        <ul className="flex flex-row justify-between">
+        <ul className="flex flex-row justify-between w-1/2">
           <li>
-            <img className="h-16 ml-3 pt-3" src="/thaparLogo.webp" alt="thapar logo" />
+            <a href="https://thapar.edu/">
+              <img className="h-16 ml-3 pt-3" src="/thaparLogo.webp" alt="thapar logo"/>
+            </a>
           </li>
-          <li className="mr-10 pt-5">
-            <button className="text-white bg-[#570f07] h-10 w-24 rounded-3xl font-semibold">LOGIN</button>
+          <li className="mt-6">
+            <a href="https://nptel.ac.in/" className="text-black">NPTEL</a>
+          </li>
+          <li className="mt-6">
+            <a href="https://cl.thapar.edu/" className="text-black">LIBRARY</a>
+          </li>
+          <li className="mt-6">
+            <a href="https://shodhratna.thapar.edu:8443/jspui/" className="text-black">ARCHIVE</a>
           </li>
         </ul>
       </div>
       <div>
         <Image className="h-80 w-screen opacity-65" src="/mHostel.jpg" alt="Photo" width={500} height={500} />
-        <h1 className="absolute top-32 pt-14 text-black text-center text-5xl font-bold" style={{left:'3%'}}>THAPAR DIGITAL CONENT MANAGEMENT SYSTEM</h1>
-        <h2 className="text-black absolute top-52 pt-6 text-center" style={{left: '4%'}}>-Empowering Education Empowering Nation</h2>
+        <div className="absolute top-32 left-5 w-3/4 py-5 bg-black opacity-65">
+          <h1 className="text-center text-5xl font-bold">THAPAR DIGITAL CONTENT MANAGEMENT SYSTEM</h1>
+          <h2 className="text-center" style={{left: '4%'}}>-Empowering Education Empowering Nation</h2>
+        </div>     
       </div>
       <div className="bg-[#eeedec] h-full">
         <div>
           <h1 className="text-black text-5xl text-center pt-20 font-serif font-medium">Unlock a World of Knowledge</h1>
           <p className="text-black text-center pt-2">Access a wide range of structured learning materials and take your academic journey to the enxt level.</p>
           <div className="flex flex-row justify-center mt-4">
-            <button className="text-white bg-[#570f07] h-10 w-48 rounded-3xl font-semibold mr-5">Browse Courses</button>
-            <button className="text-[#570f07] w-48 h-10 border-2 border-[#570f07] rounded-3xl font-semibold">Explore Confrences</button>
+            <button className="text-white bg-[#570f07] h-10 w-48 rounded-3xl font-semibold mr-5 transition duration-150 hover:bg-[#eeedec] hover:text-[#570f07] hover:border-2 hover:border-[#570f07]">Browse Courses</button>
+            <button className="text-[#570f07] w-48 h-10 border-2 border-[#570f07] rounded-3xl font-semibold transition duration-150 hover:bg-[#570f07] hover:text-[#eeedec]">Explore Confrences</button>
           </div>
         </div>
-        <div className="flex flex-row ml-48 mt-20">
+        <img src="/blooms.webp" className="opacity-10 mx-auto mt-10"/>
+        <div className="absolute flex flex-row ml-48 mt-44 top-full">
           <img className="h-96 w-96 mr-10" src="/library.jpg" alt="photo" />
           <div>
             <div className="mb-5">
@@ -47,48 +58,72 @@ export default function Home() {
         <div>
           <h1 className="font-serif text-black text-3xl text-center mt-10">About Us</h1>
           <div className="grid grid-cols-3 gap-0">
-            <div className="border border-black w-4/5 h-6/7 mt-3 ml-5 p-3 rounded-xl">
-              <div className="flex flex-row justify-between">
-                <img className="rounded-3xl h-32 w-32" src="/profile.png" alt="personal photo"/>
-                <h1 className="text-black pr-16 ml-5 pt-10 font-serif text-2xl">NAME</h1>
+            <div className="border border-black w-4/5 mt-3 ml-5 p-1 rounded-xl group hover:cursor pointer">
+              <img src="/profile.png" className="w-4/5 mx-auto transition duration-500 group-hover:opacity-45"/>
+              <div className="text-black w-1/2 relative -top-1/2 left-1/3 z-0 opacity-0 transition duration-500 group-hover:opacity-100">
+                <p>NAME</p>
+                <p>POSTION</p>
               </div>
-              <p className="text-black mt-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+              <div className="text-black mx-auto w-1/2 opacity-100 transition duration-500 group-hover:opacity-0">
+                <p className="text-center">NAME</p>
+                <p className="text-center">POSTION</p>
+              </div>
             </div>
-            <div className="border border-black w-4/5 h-6/7 mt-3 ml-5 p-3 rounded-xl">
-              <div className="flex flex-row justify-between">
-                <img className="rounded-3xl h-32 w-32" src="/profile.png" alt="personal photo"/>
-                <h1 className="text-black pr-16 ml-5 pt-10 font-serif text-2xl">NAME</h1>
+            <div className="border border-black w-4/5 mt-3 ml-5 p-1 rounded-xl group hover:cursor pointer">
+              <img src="/profile.png" className="w-4/5 mx-auto transition duration-500 group-hover:opacity-45"/>
+              <div className="text-black w-1/2 relative -top-1/2 left-1/3 z-0 opacity-0 transition duration-500 group-hover:opacity-100">
+                <p>NAME</p>
+                <p>POSTION</p>
               </div>
-              <p className="text-black mt-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+              <div className="text-black mx-auto w-1/2 opacity-100 transition duration-500 group-hover:opacity-0">
+                <p className="text-center">NAME</p>
+                <p className="text-center">POSTION</p>
+              </div>
             </div>
-            <div className="border border-black w-4/5 h-6/7 mt-3 ml-5 p-3 rounded-xl">
-              <div className="flex flex-row justify-between">
-                <img className="rounded-3xl h-32 w-32" src="/profile.png" alt="personal photo"/>
-                <h1 className="text-black pr-16 ml-5 pt-10 font-serif text-2xl">NAME</h1>
+            <div className="border border-black w-4/5 mt-3 ml-5 p-1 rounded-xl group hover:cursor pointer">
+              <img src="/profile.png" className="w-4/5 mx-auto transition duration-500 group-hover:opacity-45"/>
+              <div className="text-black w-1/2 relative -top-1/2 left-1/3 z-0 opacity-0 transition duration-500 group-hover:opacity-100">
+                <p>NAME</p>
+                <p>POSTION</p>
               </div>
-              <p className="text-black mt-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+              <div className="text-black mx-auto w-1/2 opacity-100 transition duration-500 group-hover:opacity-0">
+                <p className="text-center">NAME</p>
+                <p className="text-center">POSTION</p>
+              </div>
             </div>
-            <div className="border border-black w-4/5 h-6/7 mt-3 ml-5 p-3 rounded-xl">
-              <div className="flex flex-row justify-between">
-                <img className="rounded-3xl h-32 w-32" src="/profile.png" alt="personal photo"/>
-                <h1 className="text-black pr-16 ml-5 pt-10 font-serif text-2xl">NAME</h1>
+            <div className="border border-black w-4/5 mt-3 ml-5 p-1 rounded-xl group hover:cursor pointer">
+              <img src="/profile.png" className="w-4/5 mx-auto transition duration-500 group-hover:opacity-45"/>
+              <div className="text-black w-1/2 relative -top-1/2 left-1/3 z-0 opacity-0 transition duration-500 group-hover:opacity-100">
+                <p>NAME</p>
+                <p>POSTION</p>
               </div>
-              <p className="text-black mt-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+              <div className="text-black mx-auto w-1/2 opacity-100 transition duration-500 group-hover:opacity-0">
+                <p className="text-center">NAME</p>
+                <p className="text-center">POSTION</p>
+              </div>
             </div>
-            <div className="border border-black w-4/5 h-6/7 mt-3 ml-5 p-3 rounded-xl">
-              <div className="flex flex-row justify-between">
-                <img className="rounded-3xl h-32 w-32" src="/profile.png" alt="personal photo"/>
-                <h1 className="text-black pr-16 ml-5 pt-10 font-serif text-2xl">NAME</h1>
+            <div className="border border-black w-4/5 mt-3 ml-5 p-1 rounded-xl group hover:cursor pointer">
+              <img src="/profile.png" className="w-4/5 mx-auto transition duration-500 group-hover:opacity-45"/>
+              <div className="text-black w-1/2 relative -top-1/2 left-1/3 z-0 opacity-0 transition duration-500 group-hover:opacity-100">
+                <p>NAME</p>
+                <p>POSTION</p>
               </div>
-              <p className="text-black mt-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+              <div className="text-black mx-auto w-1/2 opacity-100 transition duration-500 group-hover:opacity-0">
+                <p className="text-center">NAME</p>
+                <p className="text-center">POSTION</p>
+              </div>
             </div>
-              <div className="border border-black w-4/5 h-6/7 mt-3 ml-5 p-3 rounded-xl">
-                <div className="flex flex-row justify-between">
-                  <img className="rounded-3xl h-32 w-32" src="/profile.png" alt="personal photo"/>
-                  <h1 className="text-black pr-16 ml-5 pt-10 font-serif text-2xl">NAME</h1>
-                </div>
-                <p className="text-black mt-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+            <div className="border border-black w-4/5 mt-3 ml-5 p-1 rounded-xl group hover:cursor pointer">
+              <img src="/profile.png" className="w-4/5 mx-auto transition duration-500 group-hover:opacity-45"/>
+              <div className="text-black w-1/2 relative -top-1/2 left-1/3 z-0 opacity-0 transition duration-500 group-hover:opacity-100">
+                <p>NAME</p>
+                <p>POSTION</p>
               </div>
+              <div className="text-black mx-auto w-1/2 opacity-100 transition duration-500 group-hover:opacity-0">
+                <p className="text-center">NAME</p>
+                <p className="text-center">POSTION</p>
+              </div>
+            </div>
           </div>
         </div>
         <div className="mt-5 bg-[#e3e3e3] h-72">
